bc020ea6e6ed39c4d24c8fd1abaac9b7e8f0da48
Guilherme Santos <guilhermess1994@gmail.com>
2022-04-13 01:51:54 -0300

    feat: add power switch function

--- src/App.tsx
+++ src/App.tsx
@@ -10,10 +10,14 @@ import { Status } from './types';
 function App() {
   const [status, setStatus] = useState<Status>('on');
 
+  const handlePowerSwitch = () => {
+    setStatus(status === 'on' ? 'off' : 'on')
+  }
+
   return (
     <div className="App">
       <Frame variant="offWhite">
-        <PowerOn variant="offWhite" status={status} />
+        <PowerOn variant="offWhite" status={status} onClick={handlePowerSwitch} />
         <Screen variant="offWhite" status={status} />
         <Controls variant="offWhite" />
         <Sound variant="offWhite" />
